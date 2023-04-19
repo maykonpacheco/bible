d8708d6848e41a99965f07fb0efa6618b2e6b41b
Maykon Pacheco <maykonpachecogeo@gmail.com>
2023-04-18 22:51:42 -0400

    add puppeteer

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const qrcode = require('qrcode-terminal');
 const axios = require('axios');
+const puppeteer = require('puppeteer');
 
 const { Client, LocalAuth } = require('whatsapp-web.js');
 
 const client = new Client({
-    authStrategy: new LocalAuth()
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+        args: ['--no-sandbox'],
+        headless: true,
+    },
 });
 
 client.on('qr', (qr) => {
