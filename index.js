7290c9a285d2368e7f9537509502c35c58f4ba1d
Maykon Pacheco <maykonpachecogeo@gmail.com>
2023-06-27 00:26:03 -0400

    add express

--- index.js
+++ index.js
@@ -1,39 +1,64 @@
+const express = require('express');
 const qrcode = require('qrcode-terminal');
 const axios = require('axios');
-const puppeteer = require('puppeteer');
 
 const { Client, LocalAuth } = require('whatsapp-web.js');
 
+const app = express();
+const port = 3008;
+
 const client = new Client({
-    authStrategy: new LocalAuth(),
-    puppeteer: {
-        args: ['--no-sandbox'],
-        headless: true,
-    },
+  authStrategy: new LocalAuth(),
+  puppeteer: {
+    args: ['--no-sandbox'],
+    headless: true,
+  },
 });
 
-client.on('qr', (qr) => {
-    qrcode.generate(qr, {small: true});
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+
+app.get('/qr', (req, res) => {
+  const qr = client.getQR();
+  qrcode.generate(qr, { small: true });
+  res.send(qr);
+});
+
+app.post('/message', async (req, res) => {
+  const { body } = req.body;
+
+  try {
+    const condition = body.indexOf('##');
+    if (condition >= 0) {
+      const verse = body.replace('##', '');
+
+      const bible = await axios(`https://bible-api.com/${verse}?translation=almeida`).then(
+        (res) => res.data
+      );
+
+      res.send(bible.text);
+    } else {
+      res.send('No verse found.');
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 client.on('ready', () => {
-    console.log('Client is ready!');
+  console.log('Client is ready!');
 });
 
-client.on('message', async msg => {
-    try {
-        const condition = msg.body.indexOf("##");
-        if (condition >= 0) {
-            const verse = msg.body.replace("##", "");
-    
-            const bible = await axios (`https://bible-api.com/${verse}?translation=almeida`)
-            .then(res => res.data);
-    
-            msg.reply(bible.text);
-        }
-      } catch (error) {
-        console.error(error);
-      }
+client.on('message', (msg) => {
+  // Handle incoming messages here
 });
 
 client.initialize();
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
